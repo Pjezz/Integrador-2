e5f05b84038e912334a96875f00fb320f59986ec
Guate27 <reypellecer@gmail.com>
2024-08-23 11:31:43 -0600

    Salon.java
    termine la clase salon

--- Salon.java
+++ Salon.java
@@ -1,3 +1,19 @@
+// CLASE PRINCIPAL 
 public class Salon {
+    // ATRIBUTOS DE LA CLASE PRINCIPAL
+    int id_salon;
+    String tipo;
+    int capacidad_maxima;
+    int costo_por_hora;
+    boolean disponibilidad;
+
+    // CONSTRUCTOR DE LA CLASE PRINCIPAL
+    public Salon(int id_salon, String tipo, int capacidad_maxima, int costo_por_hora, boolean disponibilidad) {
+        this.id_salon = id_salon;
+        this.tipo = tipo;
+        this.capacidad_maxima = capacidad_maxima;
+        this.costo_por_hora = costo_por_hora;
+        this.disponibilidad = disponibilidad;
+    }
     
-}
+    // METODO 1 DE LA CLASE PRINCIPAL:
